beb53222c2950c906bee552303d41ee13c37a6b4
Ahmed Amrou <ahmedamrou10@gmail.com>
2023-12-20 15:21:12 -0500

    added dummy data in array, assigned ids to data

--- main.py
+++ main.py
@@ -1,7 +1,31 @@
-from fastapi import FastAPI
+#API to add people and their info to a database as well as retrieve the info
+
+from typing import Optional
+from fastapi import Body, FastAPI
+from pydantic import BaseModel
+from random import randrange
 app = FastAPI()
 
-@app.get("/")
-def get_zip():
-    return "Weather"
 
+data = [{"name": "name of person", "age": "age of person", "gender": "gender of person", "id": "1"}, {"name": "Ahmed Amrou", "age": "20", "gender": "male", "id": "1"}]
+
+
+class Person(BaseModel):
+    name: str
+    age: int
+    gender: str
+    id: Optional[int] = None
+
+@app.get("/")
+def root():
+    return "Frontpage"
+
+@app.get("/people")
+def view_people():
+    return {"data": data}
+
+@app.post("/people")
+def add_person(new_person: Person):
+    new_person_dict = new_person.dict()
+    new_person_dict["id"] = randrange(1, 1000000000)
+    return new_person_dict
