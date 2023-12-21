2a23b2eb37148490c010c82a7108a3e2fc856735
Ahmed Amrou <ahmedamrou10@gmail.com>
2023-12-21 13:11:51 -0500

    Added put request functionality

--- main.py
+++ main.py
@@ -15,13 +15,13 @@ data = [{"name": "name of person", "age": "age of person", "gender": "gender of 
 
 def find_name(id):
     for p in data:
-        if p["id"] == id:
+        if p["id"] == str(id):
             return str(p)
 
         
 def find_index_name(id):
     for i, p in enumerate(data):
-        if p["id"] == id:
+        if p["id"] == str(id):
             return str(i)
 
 
@@ -41,21 +41,29 @@ def view_people():
 
 @app.post("/people", status_code=status.HTTP_201_CREATED)
 def add_person(new_person: Person):
-    new_person_dict = new_person.dict()
+    new_person_dict = new_person.model_dump()
     new_person_dict["id"] = randrange(1, 1000000000)
     return new_person_dict
 
 @app.get("/people/{id}")
 def view_person(id: int):
-    post = find_name(id)
-    if not post:
+    person = find_name(id)
+    if not person:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Post not found")
-    return post
+    return person
 
 @app.delete("/people/{id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_person(id):
-    post_index = find_index_name(id)
-    if not post_index:
+    person_index = find_index_name(id)
+    if not person_index:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Post not found")
-    data.pop(int(post_index))
+    data.pop(int(person_index))
     return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+@app.put("/people/{id}", status_code= status.HTTP_205_RESET_CONTENT)
+def update_person(id: int, new_content: Person):
+    person_index = find_index_name(id)
+    if not person_index:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Post not found")
+    data[int(person_index)]["id"],data[int(person_index)] = str(id), new_content.model_dump()
+    return data
